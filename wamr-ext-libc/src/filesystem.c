b3c346b2339089984b388add198f73d087c9ccc1
Shengyun Zhou <GGGZ-1101-28@live.cn>
2022-08-17 17:05:20 +0800

    Add fstatvfs() and statvfs() implementation

--- wamr-ext-libc/src/filesystem.c
+++ wamr-ext-libc/src/filesystem.c
@@ -8,6 +8,12 @@
 #include <sys/statvfs.h>
 #include <stdint.h>
 #include <string.h>
+#include "../internal/priv_struct_header.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-attributes"
+
+#define FS_EXT_MODULE "fs_ext"
 
 int dup(int fildes) { errno = ENOSYS; return -1; }
 int dup2(int oldfd, int newfd) { errno = ENOSYS; return -1; }
@@ -22,3 +28,46 @@ int fchmod(int fd, mode_t mode) { errno = EPERM; return -1; }
 int fchmodat(int dirfd, const char *pathname, mode_t mode, int flags) { errno = EPERM; return -1; }
 
 mode_t umask(mode_t mask) { return S_IXUSR | S_IXGRP | S_IXOTH; }
+
+struct wamr_statvfs {
+    wamr_wasi_struct_header _s_header;
+    uint32_t f_bsize;
+    uint64_t f_blocks;
+    uint64_t f_bfree;
+    uint64_t f_bavail;
+};
+
+void __wamr_statvfs_to_statvfs(const struct wamr_statvfs *internal_buf, struct statvfs *out_buf) {
+    memset(out_buf, 0, sizeof(*out_buf));
+    out_buf->f_bsize = out_buf->f_frsize = internal_buf->f_bsize;
+    out_buf->f_blocks = internal_buf->f_blocks;
+    out_buf->f_bfree = internal_buf->f_bfree;
+    out_buf->f_bavail = internal_buf->f_bavail;
+}
+
+int statvfs(const char *path, struct statvfs *buf) {
+    int tempfd = open(path, O_RDONLY);
+    if (tempfd == -1)
+        return -1;
+    int ret = fstatvfs(tempfd, buf);
+    close(tempfd);
+    return ret;
+}
+
+int32_t __imported_fd_statvfs(int32_t fd, struct wamr_statvfs *buf) __attribute__((
+    __import_module__(FS_EXT_MODULE),
+    __import_name__("fd_statvfs")
+));
+
+int fstatvfs(int fd, struct statvfs *buf) {
+    DEFINE_WAMR_WASI_STRUCT_VAR(wamr_statvfs, internal_vfs_buf);
+    int32_t err = __imported_fd_statvfs(fd, &internal_vfs_buf);
+    if (err != 0) {
+        errno = err;
+        return -1;
+    }
+    __wamr_statvfs_to_statvfs(&internal_vfs_buf, buf);
+    return 0;
+}
+
+#pragma clang diagnostic pop
